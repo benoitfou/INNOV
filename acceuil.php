0b8785467ac550cf995cb2d3b33eb44ffa8671e7
Benoitfouc <benoit.96@hotmail.fr>
2016-03-07 21:33:09 +0100

    acc

--- acceuil.php
+++ acceuil.php
@@ -2,8 +2,16 @@
 <html>
 <head>
 	<title></title>
+	<link href="css/style.css" rel="stylesheet">
 </head>
 <body>
-
+<div id="acceuil">
+	<h1>Veuillez choisir un logo :</h1>
+	<a href="?logo=1"></a><img src="image/1.png"></a>
+	<a href="?logo=2"></a><img src="image/2.png"></a>
+	<a href="?logo=3"></a><img src="image/3.png"></a>
+	<a href="?logo=4"></a><img src="image/4.png"></a>
+	<a href="?logo=5"></a><img src="image/5.png"></a>
+</div>
 </body>
 </html>
