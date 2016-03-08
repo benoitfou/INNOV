c2ae0695115d375744a3699c55485cae68bcaa37
afalcozvigne <a.falcozvigne@epsi.fr>
2016-03-08 09:14:54 +0100

    maj 9h14
    prout

--- acceuil.php
+++ acceuil.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>INNOV UP</title>
 	<link href="css/style.css" rel="stylesheet">
 </head>
 <body>
+<center><h1 id="titreEntreprise">INNOV UP</h1></center>
 <div id="acceuil">
 	<h1>Veuillez choisir un logo :</h1>
-	<a href="?logo=1"></a><img src="image/1.png"></a>
-	<a href="?logo=2"></a><img src="image/2.png"></a>
-	<a href="?logo=3"></a><img src="image/3.png"></a>
-	<a href="?logo=4"></a><img src="image/4.png"></a>
-	<a href="?logo=5"></a><img src="image/5.png"></a>
+	<a href="?logo=1"></a><img src="image/logo1.png"></a>
+	<a href="?logo=2"></a><img src="image/logo2.png"></a>
+	<a href="?logo=3"></a><img src="image/logo3.png"></a>
+	<a href="?logo=4"></a><img src="image/logo4.png"></a>
+	<a href="?logo=5"></a><img src="image/logo5.png"></a>
 </div>
 </body>
 </html>
